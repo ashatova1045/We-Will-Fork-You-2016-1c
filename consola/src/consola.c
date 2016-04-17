ce0784920d273319c6005b0f1ddfb1ba80e02bf5
Mariano Zicavo <marianozicavo@gmail.com>
2016-04-17 19:20:06 -0300

    Consola conectada

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../sockets/Sockets.h"
+#include <pthread.h>
+#include <parser/metadata_program.h>
 
-int main(void) {
-	puts("!!!Hello World!!!");
+int main(int argc, char **argv) {
+	int socket_kernel = conectar("192.168.43.246",4000);
+	if(socket_kernel == -1)
+		exit(EXIT_FAILURE);
+	getchar();
+	enviar(1,5,"hola",socket_kernel);
 	return EXIT_SUCCESS;
 }
