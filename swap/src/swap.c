4b418faea44f85692cdad0a1a0ed62c67caaac51
Nicolás Duarte <nicolasduarte93@gmail.com>
2016-04-20 17:22:37 -0300

    modificación de configuraciones

--- swap/src/swap.c
+++ swap/src/swap.c
@@ -1,14 +1,13 @@
+#include <commons/config.h>
 #include <commons/log.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <commons/config.h>
 
-#include "../../sockets/Sockets.h"
-
-int responderUMC(){
+/*int responderUMC(int *socket_memoria){
+	printf("Socket memoria: %d", *socket_memoria);
 	int socket = conectar("192.168.43.57",4200);
 	if (socket == -1)
 		exit(EXIT_FAILURE);
@@ -32,7 +31,7 @@ int responderUMC(){
 
 	close(socket);
 	return EXIT_SUCCESS;
-}
+}*/
 
 t_log* crearLog(){
 	t_log *logSwap = log_create("log.txt", "swap.c", false, LOG_LEVEL_INFO);
@@ -48,13 +47,16 @@ typedef struct{
 }t_swapcfg;
 
 t_swapcfg* levantarConfiguracion(){
-	t_swapcfg* datosSwap = (t_swapcfg*)malloc(sizeof(t_swapcfg));
-	t_config* config = config_create("../swap/swap.cfg");
-	datosSwap->puerto_escucha = config_get_int_value(config,"PUERTO_ESCUCHA");
-	datosSwap->nombre_swap = config_get_string_value(config, "NOMBRE_SWAP");
-	datosSwap->cantidad_paginas = config_get_int_value(config, "CANTIDAD_PAGINAS");
-	datosSwap->tamanio_pagina = config_get_int_value(config, "TAMANIO_PAGINA");
-	datosSwap->retardo_compactacion = config_get_int_value(config, "RETARDO_COMPACTACION");
+	t_swapcfg* datosSwap = malloc(sizeof(t_swapcfg));
+	if(datosSwap != NULL){
+		t_config* config = config_create("../swap/swap.cfg");
+		datosSwap->puerto_escucha = config_get_int_value(config,"PUERTO_ESCUCHA");
+		datosSwap->nombre_swap = config_get_string_value(config, "NOMBRE_SWAP");
+		datosSwap->cantidad_paginas = config_get_int_value(config, "CANTIDAD_PAGINAS");
+		datosSwap->tamanio_pagina = config_get_int_value(config, "TAMANIO_PAGINA");
+		datosSwap->retardo_compactacion = config_get_int_value(config, "RETARDO_COMPACTACION");
+		config_destroy(config);
+	}
 	return datosSwap;
 }
 
@@ -69,33 +71,34 @@ int main(int argc, char** argv) {
 	printf("Se conecta al puerto %d \n",config_swap->puerto_escucha);
 
 	// Inicializa la conexión y queda a espera de procesos UMC
-	int socketserver = crear_socket_escucha(4100);
+	int socketserver = crear_socket_escucha(config_swap->puerto_escucha);
+	//int socketserver = crear_socket_escucha(4100);
 	if (socketserver == -1)
 		exit(EXIT_FAILURE);
 
 	puts("Esperando procesos UMC...");
 
 	// Acepta la conexón de un proceso UMC
-	int socket_conectado =  aceptar_cliente(socketserver);
+	int socket_memoria =  aceptar_cliente(socketserver);
 
-	if(socket_conectado == -1)
+	if(socket_memoria == -1)
 		exit(EXIT_FAILURE);
 
-	printf("Se conectó el UMC con socket: %d \n",socket_conectado);
+	printf("Se conectó el UMC con socket: %d \n",socket_memoria);
 
-	t_paquete* paquete;
+//	t_paquete* paquete;
 
 	// Recibe la petición
-	paquete = recibir_paquete(socket_conectado);
-	if(paquete->cod_op == ERROR_COD_OP)
-		exit(EXIT_FAILURE);
+	//paquete = recibir_paquete(socket_memoria);
+	//	exit(EXIT_FAILURE);
+	//if(paquete->cod_op == ERROR_COD_OP)
 
 	//uint16_t cod_op = paquete->cod_op;
  	//char *datos = paquete->datos;
-	puts("Proceso recibido");
-	printf("Codigo de operacion: %d\n",paquete->cod_op);
-	printf("Tamano de los datos: %d\n",paquete->tamano_datos);
-	printf("Datos: %s\n",(char*)paquete->datos);
+	//puts("Proceso recibido");
+	//printf("Codigo de operacion: %d\n",paquete->cod_op);
+	//printf("Tamano de los datos: %d\n",paquete->tamano_datos);
+	//printf("Datos: %s\n",(char*)paquete->datos);
 
 	//todo: Asignar tamaño necesario para el proceso en caso de solicitarse
 	//todo: Compactar partición en caso de fragmentación
@@ -106,11 +109,11 @@ int main(int argc, char** argv) {
 
 	//todo: Liberar espacio en caso que se finalice el proceso
 
-//	responderUMC();
+//	responderUMC(&socket_memoria);
 
-	destruir_paquete(paquete);
+	//destruir_paquete(paquete);
 
 	close(socketserver);
-	close (socket_conectado);
+	close (socket_memoria);
 	return EXIT_SUCCESS;
 }
