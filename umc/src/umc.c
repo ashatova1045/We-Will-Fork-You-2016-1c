0b701a8036efef875fe376d6cb6f62e2d7ec04b5
ElianaFlorian <eliflorian_06@hotmail.com>
2016-04-20 03:30:10 -0300

    Archivo de configuracion de la umc

--- umc/src/umc.c
+++ umc/src/umc.c
@@ -1,11 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/log.h>//Incluyo commons
-#include <commons/collections/list.h> //Incluyo commons
 #include <commons/string.h>
 #include <parser/metadata_program.h>//Incluyo el parser
 #include "../../sockets/Sockets.h"
+#include <stdbool.h>
+#include <unistd.h>
+#include <commons/config.h>
 
+//Defino la funcion para crear el log
+t_log* crearLog(){
+	t_log* logUMC = log_create("Log.txt","umc.c",false,LOG_LEVEL_INFO);
+	return logUMC;
+}
+
+//Creo la estructura de configuración
+typedef struct{
+	int puerto;
+	char* ip_swap;
+	int puerto_swap;
+	int cant_marcos;
+	int marco_size;
+	int marco_x_proc;
+	int entradas_tlb;
+	int retardo;
+}t_umcConfig;
+
+t_umcConfig* leerConfiguracion(){
+	//Asigno memoria para estructura de configuración
+	t_umcConfig* datosUmc = (t_umcConfig*)malloc(sizeof(t_umcConfig));
+
+	//Creo el archivo de configuración
+	t_config* config = config_create("../umc/umc.cfg");
+
+	//Completo los datos de la umc con los valores del archivo de configuración
+	datosUmc->puerto = config_get_int_value(config,"PUERTO");
+	datosUmc->ip_swap = config_get_string_value(config,"IP_SWAP");
+	datosUmc->puerto_swap = config_get_int_value(config,"PUERTO_SWAP");
+	datosUmc->cant_marcos = config_get_int_value(config,"CANT_MARCOS");
+	datosUmc->marco_size = config_get_int_value(config,"MARCO_SIZE");
+	datosUmc->marco_x_proc = config_get_int_value(config,"MARCO_X_PROC");
+	datosUmc->entradas_tlb = config_get_int_value(config,"ENTRADAS_TLB");
+	datosUmc->retardo = config_get_int_value(config,"RETARDO");
+
+	//Devuelvo la estructura de datos cargados
+	return datosUmc;
+}
+
+//Creo el entero para referenciar al socket del swap
 int socketswap;
 
 //Función para manejar los mensajes
@@ -30,6 +72,13 @@ void nueva_conexion(int socket){
 //Creo el socket al que le voy a mandar el mensaje
 
 int main(int argc, char **argv){
+	//Creo el archivo log
+	t_log* logUMC = crearLog();
+	log_info(logUMC,"Ejecución del proceso UMC");
+
+	//Leo la configuración de la memoria
+	t_umcConfig* config_umc = leerConfiguracion();
+	printf("Puerto de conexion %d\n",config_umc->puerto);
 
 	//TODO Leer archivo de configuración y solicitar un bloque de memoria contigua
 
@@ -50,7 +99,7 @@ int main(int argc, char **argv){
 	//Me conecto al área de swap
 	socketswap = conectar("192.168.43.188",4100);
 
-	//Creo el server multiconección
+	//Creo el server multiconexión
 	fd_set set_de_fds;
 	int* fdmax = malloc(sizeof(int));
 	if (fdmax==NULL){
