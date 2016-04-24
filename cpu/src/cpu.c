5983ae7fac6e42a0c0402c8512cc71d677568a04
lucasgiuliano88 <lucasgiuliano88@gmail.com>
2016-04-24 12:30:54 -0300

    Agrego logs

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -11,16 +11,18 @@
 #include <stdlib.h>
 #include <commons/config.h>
 #include <commons/log.h>
+#include <errno.h>
 #include <commons/collections/list.h>
 #include "../../sockets/Sockets.h"
 
 int socket_umc,socket_nucleo;
+t_log* logcpu;
 //Defino la función para leer la configuración
 
 void conectar_cpu() {
-	t_config* conf = config_create("cpu.cfg");
+	t_config* conf = config_create("../cpu/cpu.cfg");
 	if (!conf) {
-		//      log_error(logCpu,"Error al abrir la configuracion");
+		log_error(logcpu,"Error al abrir la configuracion");
 		puts("Fallo config");
 			  exit(EXIT_FAILURE);
 	}
@@ -28,15 +30,15 @@ void conectar_cpu() {
 	socket_nucleo = conectar(config_get_string_value(conf, "IP_NUCLEO"),
 			config_get_int_value(conf, "PUERTO_NUCLEO"));
 	if (socket_nucleo == -1) {
-		//  log_error(logCpu,"Error al conectarse al server! %s\n IP: %s PUERTO: %d",strerror(errno),config_get_string_value(conf,"IP_KERNEL"),config_get_int_value(conf,"PUERTO_KERNEL"));
-		//  exit(EXIT_FAILURE);
+		log_error(logcpu,"Error al conectarse al nucleo!");
+		  exit(EXIT_FAILURE);
 	}
 
 	socket_umc = conectar(config_get_string_value(conf, "IP_UMC"),
 								config_get_int_value(conf, "PUERTO_UMC"));
 	if (socket_umc == -1) {
-		//  log_error(logCpu,"Error al conectarse al server! %s\n IP: %s PUERTO: %d",strerror(errno),config_get_string_value(conf,"IP_KERNEL"),config_get_int_value(conf,"PUERTO_KERNEL"));
-		//  exit(EXIT_FAILURE);
+		log_error(logcpu,"Error al conectarse a la umc!");
+		  exit(EXIT_FAILURE);
 	}
 
 //Elimino la configuración que ya no necesito
@@ -44,6 +46,11 @@ void conectar_cpu() {
 }
 
 int main() {
+	//Creo archivo de log
+	logcpu = log_create("logcpu.log", "cpu", false, LOG_LEVEL_DEBUG);
+
 	conectar_cpu();
+	log_destroy(logcpu);
+	log_debug(logcpu, "");
 	return (EXIT_SUCCESS);
 }
