e33c15d0c5a56573d227c28b3517f01e1175ddab
Mariano Zicavo <marianozicavo@gmail.com>
2016-04-24 14:14:48 -0300

    Corrijo error en el handshake

--- sockets/Sockets.c
+++ sockets/Sockets.c
@@ -14,8 +14,8 @@ int handshake(int socket, uint16_t cop_op_cliente, uint16_t cod_op_esperado)
 	struct timeval timeout;
 	timeout.tv_sec = 5;
 	FD_ZERO(&set);
-	FD_SET(socket, set);
-	select(socket+1,&set,NULL,NULL,timeout);
+	FD_SET(socket, &set);
+	select(socket+1,&set,NULL,NULL,&timeout);
 	if(FD_ISSET(socket,&set)){
 		t_paquete *paquete = recibir_paquete(socket);
 		if(paquete->cod_op == cod_op_esperado)
