d53c8c672df712f4ef35b377f61e63c7c7e5167c
lucasgiuliano88 <lucasgiuliano88@gmail.com>
2016-04-24 11:56:04 -0300

    Lectura de config

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -1,29 +1,49 @@
+/*
+ ============================================================================
+ Name        : abrir.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
 #include <stdio.h>
 #include <stdlib.h>
-#include <parser/metadata_program.h>
-#include "/home/utnso/workspace/tp-2016-1c-We-Will-Fork-You/sockets/Sockets.h"
-#include <commons/string.h>
+#include <commons/config.h>
+#include <commons/log.h>
 #include <commons/collections/list.h>
+#include "../../sockets/Sockets.h"
 
-int main(void) {
+int socket_umc,socket_nucleo;
+//Defino la función para leer la configuración
 
-	//Me conecto al kernel y la umv
-	int kernel = conectar("192.168.43.246", 4050);
-	int umv = conectar("192.168.43.57", 4200);
+void conectar_cpu() {
+	t_config* conf = config_create("cpu.cfg");
+	if (!conf) {
+		//      log_error(logCpu,"Error al abrir la configuracion");
+		puts("Fallo config");
+			  exit(EXIT_FAILURE);
+	}
 
-	if (kernel == -1 || umv == -1)
-	exit (EXIT_FAILURE);
+	socket_nucleo = conectar(config_get_string_value(conf, "IP_NUCLEO"),
+			config_get_int_value(conf, "PUERTO_NUCLEO"));
+	if (socket_nucleo == -1) {
+		//  log_error(logCpu,"Error al conectarse al server! %s\n IP: %s PUERTO: %d",strerror(errno),config_get_string_value(conf,"IP_KERNEL"),config_get_int_value(conf,"PUERTO_KERNEL"));
+		//  exit(EXIT_FAILURE);
+	}
 
-	//Me preparo para recibir los paquetes
-	t_paquete *paquete = recibir_paquete(kernel);
+	socket_umc = conectar(config_get_string_value(conf, "IP_UMC"),
+								config_get_int_value(conf, "PUERTO_UMC"));
+	if (socket_umc == -1) {
+		//  log_error(logCpu,"Error al conectarse al server! %s\n IP: %s PUERTO: %d",strerror(errno),config_get_string_value(conf,"IP_KERNEL"),config_get_int_value(conf,"PUERTO_KERNEL"));
+		//  exit(EXIT_FAILURE);
+	}
 
-	puts(paquete->datos);
+//Elimino la configuración que ya no necesito
+	config_destroy(conf);
+}
 
-	//Reenvío
-	enviar(1, paquete->tamano_datos, paquete->datos, umv);
-
-	//Destruyo el paquete enviado
-	destruir_paquete(paquete);
-
+int main() {
+	conectar_cpu();
 	return (EXIT_SUCCESS);
 }
