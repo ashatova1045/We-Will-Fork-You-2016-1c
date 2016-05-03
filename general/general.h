511662354bdd5d2d5e45a5e8506d2fc98bb26e1b
Anastasia <a.shatova1045@gmail.com>
2016-05-02 21:26:57 -0300

    Armando nuevo pcb y cambios al general.h

--- general/general.h
+++ general/general.h
@@ -2,21 +2,13 @@
 #define GENERAL_H_
 
 #include <sys/types.h>
+#include <parser/metadata_program.h>
 
 typedef struct
 {
 	char nombre;
 	int32_t valor;
 }t_variable_completa;
-
-typedef struct {
-	int32_t pid;
-	int32_t pc;
-	int32_t pag_codigo;
-	int32_t indice_codigo;
-	int32_t indice_etiquetas;
-	int32_t fin_stack;
-}t_pcb;
 
 typedef struct {
 	int32_t pag;
@@ -36,5 +28,19 @@ typedef struct {
 	int32_t pos_retorno;
 	t_posMemoria * pos_var_retorno;
 } registro_indice_stack;
+
+typedef struct {
+	int32_t pid;
+	int32_t pc;
+	int32_t pag_codigo;
+	t_intructions* indice_codigo;
+	int32_t indice_etiquetas;
+	int32_t cant_etiquetas;
+	int32_t fin_stack;
+	u_int32_t cant_instrucciones;
+	registro_indice_stack* indice_stack;
+}t_pcb;
+
+
 
 #endif /* GENERAL_H_ */
