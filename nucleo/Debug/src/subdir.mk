0bb5860ef91f8638ee7c36dbfc5b680b3d01db64
Anastasia <a.shatova1045@gmail.com>
2016-05-06 17:37:37 -0300

    subo las colas de los 5 estados, crearlas y mover de/hacia cola

--- nucleo/Debug/src/subdir.mk
+++ nucleo/Debug/src/subdir.mk
@@ -4,12 +4,15 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../src/estados.c \
 ../src/nucleo.c 
 
 OBJS += \
+./src/estados.o \
 ./src/nucleo.o 
 
 C_DEPS += \
+./src/estados.d \
 ./src/nucleo.d 
 
 
