ca74da570a7b1fbc33a4c434074f3bf4dcff3bde
Nicolás Duarte <nicolasduarte93@gmail.com>
2016-05-22 22:54:24 -0300

    subo cosas que modifiqué antes que se me haya corrompido el archivo git

--- cpu/src/primitivas.h
+++ cpu/src/primitivas.h
@@ -26,6 +26,11 @@ typedef struct{
 	t_valor_variable valor;
 }t_asignar;
 
+typedef struct{
+	t_nombre_compartida variable;
+	t_valor_variable valor_variable;
+}t_asignar_var_comp;
+
 AnSISOP_funciones functions;
 AnSISOP_kernel kernel_functions;
 
@@ -34,10 +39,10 @@ t_posicion* definirVariable(t_nombre_variable identificador_variable);
 t_posicion* obtenerPosicionVariable(t_nombre_variable identificador_variable);
 t_valor_variable dereferenciar(t_posicion* direccion_variable);
 void asignar(t_posicion	direccion_variable,	t_valor_variable valor);
-//todo:t_valor_variable	obtenerValorCompartida(t_nombre_compartida	variable);
-//todo:t_valor_variable asignarValorCompartida(t_nombre_compartida	variable, t_valor_variable valor_variable);
+t_valor_variable	obtenerValorCompartida(t_nombre_compartida	variable);
+t_valor_variable asignarValorCompartida(t_nombre_compartida	variable, t_valor_variable valor_variable);
 t_puntero_instruccion irAlLabel(t_nombre_etiqueta etiqueta);
-//todo:t_puntero_instruccion llamarFuncion(t_nombre_etiqueta etiqueta, t_posicion donde_retornar, t_puntero_instruccion linea_en_ejecuccion);
+void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar);
 //todo:t_puntero_instruccion retornar(t_valor_variable retorno);
 int	imprimir(t_valor_variable valor_mostrar);
 void imprimirTexto(char*);
