73aa154e25bde74607969454fe272fa64041a588
ElianaFlorian <eliflorian_06@hotmail.com>
2016-06-01 23:19:01 -0300

    Subo makefile

--- umc/Debug/objects.mk
+++ umc/Debug/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := ../../general/Operaciones_umc.c ../../general/operaciones_swap.c ../../sockets/Sockets.c
+USER_OBJS := ../../general/Operaciones_umc.c ../../general/funciones_listas.c ../../general/operaciones_swap.c ../../sockets/Sockets.c
 
 LIBS := -lpthread -lcommons
 
