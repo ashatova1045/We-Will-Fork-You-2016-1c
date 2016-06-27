372b7263851bb83ca77ca8880317beaa6dcafdf6
Mariano Zicavo <marianozicavo@gmail.com>
2016-06-27 17:31:32 -0300

    Agrego script para setear IPs mas rapido

---
+++ scripts/setear-ips.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Seteando IPs"
+echo "IP SWAP:"
+read ip_swap
+find ../configuraciones/* -type f -exec \
+    sed -i 's/^IP_SWAP=.*$/IP_SWAP='$ip_swap'/g' {} +
+
+echo "IP UMC:"
+read ip_umc
+find ../configuraciones/* -type f -exec \
+    sed -i 's/^IP_UMC=.*$/IP_UMC='$ip_umc'/g' {} +
+
+echo "IP NUCLEO:"
+read ip_nucleo
+find ../configuraciones/* -type f -exec \
+    sed -i 's/^IP_NUCLEO=.*$/IP_NUCLEO='$ip_nucleo'/g' {} +
+
+
+sed -i 's/^IP_NUCLEO=.*$/IP_NUCLEO='$ip_nucleo'/g' ../consola/consola.cfg
+
+sed -i 's/^IP_UMC=.*$/IP_UMCP='$ip_umc'/g' ../cpu/cpu.cfg
+sed -i 's/^IP_NUCLEO=.*$/IP_NUCLEO='$ip_nucleo'/g' ../cpu/cpu.cfg
+
+echo "Fin"
